b892bca470365ae18919f6c23542179a088ca607
Peter Martian <petmartian@gmail.com>
2025-02-27 15:27:46 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="Martian_Store_Dashboard!!!", page_icon=":bar_chart:", layout="wide")
+
+st.title(" :bar_chart : Martian Store EDA")
