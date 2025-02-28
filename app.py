81e6cf492387f0801423abbfd778f828827b7108
Peter Martian <petmartian@gmail.com>
2025-02-28 16:54:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -171,16 +171,3 @@ with st.expander("Summary_Table"):
     st.write(sub_category_Year.style.background_gradient(cmap="Blues"))
 
 
-# Create a scatter plot
-data1 = px.scatter(filtered_df, x="Sales", y="Profit", size="Quantity")
-data1['layout'].update(
-    title="Relationship between Sales and Profits using Scatter Plot.",
-    titlefont=dict(size=20),
-    xaxis=dict(title="Sales", titlefont=dict(size=19)),
-    yaxis=dict(title="Profit", titlefont=dict(size=19))
-)
-st.plotly_chart(data1, use_container_width=True)
-
-# Expander for viewing the data
-with st.expander("View Data"):
-    st.write(filtered_df.iloc[:500, 1:20:2].style.background_gradient(cmap="Oranges"))
