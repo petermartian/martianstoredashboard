8c91a228b270a8e3d58a98b0e127e78f18b9464e
Peter Martian <petmartian@gmail.com>
2025-02-28 15:53:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,11 @@ st.plotly_chart(data1,use_container_width=True)
 with st.expander("View Data"):
     st.write(filtered_df.iloc[:500,1:20:2].style.background_gradient(cmap="Oranges"))
 
-# Download orginal DataSet
-csv = df.to_csv(index = False).encode('utf-8')
-st.download_button('Download Data', data = csv, file_name = "Data.csv",mime = "text/csv")
-
+# Download original DataSet as CSV
+csv = df.to_csv(index=False).encode('utf-8')
+st.download_button(
+    label='Download Data', 
+    data=csv, 
+    file_name="Data.csv", 
+    mime_type="text/csv"
+)
