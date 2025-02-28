a60cc11d39d5d95de8dfb967b94e5f0ca8220ba8
Peter Martian <petmartian@gmail.com>
2025-02-28 15:28:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ with col2:
     fig.update_traces(text = filtered_df["Region"], textposition = "outside")
     st.plotly_chart(fig,use_container_width=True)
 
-# We want to download the report to CSV, Excel
+# We want to download the report to CSV
 
 cl1, cl2 = st.columns((2))
 with cl1:
@@ -116,4 +116,19 @@ with cl2:
         csv = region.to_csv(index = False).encode('utf-8')
         st.download_button("Download Data", data = csv, file_name = "Region.csv", mime = "text/csv",
                         help = 'Click here to download the data as a CSV file')
+
+# Creating a time series chart using line chart
+filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
+st.subheader('Time Series Analysis')
+
+# Defining the line chart
+linechart = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+fig2 = px.line(linechart, x = "month_year", y="Sales", labels = {"Sales": "Amount"},height=500, width = 1000,template="gridon")
+st.plotly_chart(fig2,use_container_width=True)
+
+# Downloading the data into CSV
+with st.expander("View Data of TimeSeries:"):
+    st.write(linechart.T.style.background_gradient(cmap="Blues"))
+    csv = linechart.to_csv(index=False).encode("utf-8")
+    st.download_button('Download Data', data = csv, file_name = "TimeSeries.csv", mime ='text/csv')
 
